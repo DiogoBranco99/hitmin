3646a08365f77da69bbb2a575ccb765ca2fe0967
David Silva <davidmssilva5@gmail.com>
2021-01-30 17:17:35 +0000

    PlayerSpawn esta. Outros spawns por fazer + bug main menu

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -19,8 +19,9 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame 
     void Update()
     {
+        //playerBody = GameObject.FindWithTag("Player").transform;
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime * 1.5f;
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
---
+++ Assets/Scripts/SpawnPointPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointPlayer : MonoBehaviour
+{
+    public GameObject[] spawnLocations;
+    public GameObject player;
+/*    public Transform navmesh;
+    public HealthBar healthbar;*/
+    private Vector3 respawnLocation;
+
+    void Awake()
+    {
+        spawnLocations = GameObject.FindGameObjectsWithTag("SpawnerPointPlayer");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*       player = (GameObject)Resources.Load("Player", typeof(GameObject));
+               navmesh = GameObject.FindGameObjectWithTag("NavmeshTag").transform;
+               healthbar = FindObjectOfType<PlayerHealth>().healthBar;
+
+               player.AddComponent<PlayerMovement>().groundCheck = navmesh;
+               player.AddComponent<PlayerHealth>().healthBar = healthbar;*/
+
+
+        player = (GameObject)GameObject.FindGameObjectWithTag("Player");
+        
+        SpawnPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void SpawnPlayer()
+    {
+        int spawn = Random.Range(0, spawnLocations.Length);
+        /*     GameObject.Instantiate(player, spawnLocations[spawn].transform.position, Quaternion.identity);
+             Destroy(GameObject.FindGameObjectWithTag("Player"));*/
+        player.transform.position = spawnLocations[spawn].transform.position;
+    }
+}
