ad2dc52baeebaafd242fc2f46346bf13c40dc3ed
David Silva <davidmssilva5@gmail.com>
2021-01-31 16:27:20 +0000

    spawners funcionam todos menos o do enemy. possivel conflito com o enemyAI

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    private GameObject[] spawnLocations;
+    private GameObject gObject;
+    private GameObject[] gObjects;
+    private string tagToFindObject;
+    private string tagToFindLocations;
+    private string parentCallingName;
+    private bool findSingular = true;
+
+    void Awake()
+    {
+        
+        parentCallingName = gameObject.name;
+        Debug.Log(parentCallingName);
+
+        switch (parentCallingName){
+            case "SpawnersPlayer":
+                tagToFindLocations = "SpawnerPointPlayer";
+                tagToFindObject = "Player";
+                findSingular = true;
+                break;
+            case "SpawnersEnemy":
+                tagToFindLocations = "SpawnerPointEnemy";
+                tagToFindObject = "Enemy";
+                findSingular = true;
+                break;
+            case "SpawnersClues":
+                tagToFindLocations = "SpawnerPointClues";
+                tagToFindObject = "Clue";
+                findSingular = false;
+                break;
+            case "SpawnersNPCs":
+                tagToFindLocations = "SpawnerPointNPC";
+                tagToFindObject = "NPC";
+                findSingular = false;
+                break;
+            case "SpawnersVictim":
+                tagToFindLocations = "SpawnerPointVictim";
+                tagToFindObject = "Victim";
+                findSingular = true;
+                break;
+            case "SpawnersExchange":
+                tagToFindLocations = "SpawnerPointExch";
+                tagToFindObject = "ExchangePoint";
+                findSingular = true;
+                break;
+            default:
+                break;
+        }
+
+        spawnLocations = GameObject.FindGameObjectsWithTag(tagToFindLocations);
+        Debug.Log(spawnLocations);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (findSingular){
+            Debug.Log(tagToFindObject);
+            gObject = (GameObject)GameObject.FindGameObjectWithTag(tagToFindObject);
+            SpawnObj();
+        } else {
+            gObjects = GameObject.FindGameObjectsWithTag(tagToFindObject);
+            SpawnObjects();
+        }
+
+    }
+
+    private void SpawnObj()
+    {
+        int spawn = Random.Range(0, spawnLocations.Length);
+        gObject.transform.position = spawnLocations[spawn].transform.position;
+        Debug.Log(spawnLocations[spawn].transform.position);
+    }
+
+    private void SpawnObjects()
+    {
+        int spawn;
+        var locationToSpawn = new GameObject();
+        for (int i = 0; i< gObjects.Length; i++){
+            spawn = Random.Range(0, spawnLocations.Length);
+            gObject = gObjects[i];
+            locationToSpawn = spawnLocations[spawn];
+
+            //transforms position
+            gObject.transform.position = locationToSpawn.transform.position;
+
+            //converts to list to remove index
+            var tempList = toList(spawnLocations);
+            tempList.Remove(locationToSpawn);
+            spawnLocations = tempList.ToArray();
+        }
+    }
+
+    private List<GameObject> toList(GameObject[] mArray)
+    {
+        var rList = new List<GameObject>();
+
+        for(int i=0; i< mArray.Length; i++)
+        {
+            rList.Add(mArray[i]);
+        }
+
+        return rList;
+    }
+}
---
+++ Assets/Scripts/SpawnPointEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointEnemy : MonoBehaviour
+{
+    private GameObject[] spawnLocations;
+    private GameObject enemy;
+
+    void Awake()
+    {
+        spawnLocations = GameObject.FindGameObjectsWithTag("SpawnerPointEnemy");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = (GameObject)GameObject.FindGameObjectWithTag("Enemy");
+
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        int spawn = Random.Range(0, spawnLocations.Length);
+        enemy.transform.position = spawnLocations[spawn].transform.position;
+    }
+}
